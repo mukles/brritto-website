21d4d5c1be3acaf7d146dc70142c5d1eace78ea7
mukles <muklesdev@gmail.com>
2026-01-11 18:16:39 +0600

    Replace R2 incremental cache with static assets incremental cache in configuration

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,10 +1,6 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import staticAssetsIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/static-assets-incremental-cache";
 
 export default defineCloudflareConfig({
-  // Uncomment to enable R2 cache,
-  // It should be imported as:
-  // See https://opennext.js.org/cloudflare/caching for more details
-  incrementalCache: r2IncrementalCache,
+  incrementalCache: staticAssetsIncrementalCache,
 });
