aaa3352bb679a7986dc7dcd44149d83330ce06fd
mukles <muklesdev@gmail.com>
2026-01-11 18:38:02 +0600

    Refactor caching configuration: integrate regional cache and update tag cache implementation

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,19 +1,19 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
-import d1NextTagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
+import { withRegionalCache } from "@opennextjs/cloudflare/overrides/incremental-cache/regional-cache";
+import doShardedTagCache from "@opennextjs/cloudflare/overrides/tag-cache/do-sharded-tag-cache";
 import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
+import { purgeCache } from "@opennextjs/cloudflare/overrides/cache-purge/index";
 
 export default defineCloudflareConfig({
-  // R2 for incremental cache storage (ISR/SSG data)
-  incrementalCache: r2IncrementalCache,
-
-  // Durable Objects Queue for time-based revalidation
+  incrementalCache: withRegionalCache(r2IncrementalCache, {
+    mode: "long-lived",
+  }),
   queue: doQueue,
-
-  // D1 Tag Cache for on-demand revalidation (revalidateTag/revalidatePath)
-  tagCache: d1NextTagCache,
-
-  // Enable cache interception for better performance on cached routes
-  // Disable this if you use PPR
+  // This is only required if you use On-demand revalidation
+  tagCache: doShardedTagCache({ baseShardSize: 12 }),
+  // Disable this if you want to use PPR
   enableCacheInterception: true,
+  // you can also use the `durableObject` option to use a durable object as a cache purge
+  cachePurge: purgeCache({ type: "direct" }),
 });
